04a5e37e916098ad6d732832e45922299a83107b
axhse <asbityugov@edu.hse.ru>
2023-07-11 14:45:11 +0300

    configure to production

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ COPY . /app/
 
 EXPOSE 8000
 
+RUN python manage.py collectstatic --noinput
+
 ENTRYPOINT ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b8=wslefjgoo&$m=^i*y76^@v!j@n@76rksx5y)9hldsmruxc3"
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", secrets.token_hex(100))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # TODO: change to False
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = "static/"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
